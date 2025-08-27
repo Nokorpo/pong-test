81ce21016ab8f5a1a3c16744893e8df4590b52b8
AnaRocioCordobesArevalo <annarca.6@gmail.com>
2025-08-27 18:10:41 +0200

    Create first test

--- tests/test_starts.gd
+++ tests/test_starts.gd
@@ -10,3 +10,26 @@ func test_game_starts():
 	# THEN
 	var ball: Node2D = game.find_child("Pelota")
 	assert_not_null(ball, "la pelota no existe")
+
+func test_pala_exists():
+	# GIVEN
+	var game: Node2D = add_child_autofree(load("res://main.tscn").instantiate())
+
+	# WHEN
+	#it is loaded
+
+	# THEN
+	var pala: Node2D = game.find_child("Pala")
+	assert_not_null(pala, "la pala no existe")
+
+
+func test_pala2_exists():
+	# GIVEN
+	var game: Node2D = add_child_autofree(load("res://main.tscn").instantiate())
+
+	# WHEN
+	#it is loaded
+
+	# THEN
+	var pala2: Node2D = game.find_child("Pala2")
+	assert_not_null(pala2, "la pala2 no existe")
