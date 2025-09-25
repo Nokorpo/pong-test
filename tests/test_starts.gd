bf773cd3d028535a4d67d201387a28bb9505c872
Edearth <nepormoseno1992@gmail.com>
2025-09-25 16:27:52 +0200

    Simplify test code

--- tests/test_starts.gd
+++ tests/test_starts.gd
@@ -1,27 +1,24 @@
 extends GutTest
 
-func test_game_starts():
-	# GIVEN
-	var game: Node2D = load("res://main.tscn").instantiate()
-	add_child(game)
+class TestGame extends GutTest:
 
-	# WHEN
-	#it is loaded
+	var game: Node2D
 
-	# THEN
-	assert_not_null(game, "the game couldn't be started")
-	game.free()
+	func before_all():
+		# GIVEN
+		game = load("res://main.tscn").instantiate()
+		add_child(game)
 
-func test_paddle_exists():
-	# GIVEN
-	var game: Node2D = load("res://main.tscn").instantiate()
-	add_child(game)
+	func after_all():
+		game.free()
 
-	# WHEN
-	#it is loaded
+	func test_game_starts():
+		# WHEN it is loaded
+		# THEN
+		assert_not_null(game, "the game couldn't be started")
 
-	# THEN
-	var paddle: Node2D = game.find_child("Pala")
-	assert_not_null(paddle, "the paddle does not exist")
-
-	game.free()
+	func test_paddle_exists():
+		# WHEN it is loaded
+		# THEN
+		var paddle: Node2D = game.find_child("Pala")
+		assert_not_null(paddle, "the paddle does not exist")
