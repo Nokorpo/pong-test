86f1f38d73b1097dda189b135cf64e0e2588c6e3
Edearth <nepormoseno1992@gmail.com>
2025-09-25 16:24:34 +0200

    Implement first tests

--- tests/test_starts.gd
+++ tests/test_starts.gd
@@ -2,11 +2,26 @@ extends GutTest
 
 func test_game_starts():
 	# GIVEN
-	var game: Node2D = add_child_autofree(load("res://main.tscn").instantiate())
+	var game: Node2D = load("res://main.tscn").instantiate()
+	add_child(game)
 
 	# WHEN
 	#it is loaded
 
 	# THEN
-	var ball: Node2D = game.find_child("Pelota")
-	assert_not_null(ball, "la pelota no existe")
+	assert_not_null(game, "the game couldn't be started")
+	game.free()
+
+func test_paddle_exists():
+	# GIVEN
+	var game: Node2D = load("res://main.tscn").instantiate()
+	add_child(game)
+
+	# WHEN
+	#it is loaded
+
+	# THEN
+	var paddle: Node2D = game.find_child("Pala")
+	assert_not_null(paddle, "the paddle does not exist")
+
+	game.free()
