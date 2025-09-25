9eeef6f57a6b66363744d1267fd88fd54f447ef3
Edearth <nepormoseno1992@gmail.com>
2025-09-25 16:41:52 +0200

    Add ball integration tests

---
+++ tests/test_waits.gd
@@ -0,0 +1,44 @@
+extends GutTest
+
+class TestIntegrationBall extends GutTest:
+
+	# sut = System Under Test
+	var sut: PackedScene = load("res://main.tscn")
+
+	func test_ball_exists():
+		# GIVEN
+		var game: Node2D = add_child_autofree(sut.instantiate())
+
+		# WHEN
+		# game starts
+
+		var ball: Node2D = game.find_child("Pelota")
+		assert_not_null(ball, "the ball does not exist")
+
+	func test_ball_moves_on_start():
+		# GIVEN
+		var game: Node2D = add_child_autofree(sut.instantiate())
+		var ball: Node2D = game.find_child("Pelota")
+		var initial_position: Vector2 = ball.position
+
+		# WHEN
+		await wait_seconds(1)
+
+		# THEN
+		assert_ne(ball.position, initial_position, "the ball didn't move")
+		assert_gt(ball.position, initial_position, "the ball didn't move in the right direction")
+
+	func test_ball_can_score():
+		# GIVEN
+		var game: Node2D = add_child_autofree(sut.instantiate())
+		var ball: Node2D = game.find_child("Pelota")
+		ball.position = Vector2(1000, 120)
+		var score_label: Label = game.find_child("Label")
+		var initial_score := score_label.text
+
+		# WHEN
+		await wait_seconds(1)
+
+		# THEN
+		assert_ne(score_label.text, initial_score, "the score didn't change")
+		assert_gt(score_label.text, initial_score, "the score didn't increase")
